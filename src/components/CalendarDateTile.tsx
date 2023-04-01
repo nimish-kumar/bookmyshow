57404ecf15024130eb13c755347e913d735ad182
Nimish Kumar <nimishk642@gmail.com>
2023-04-01 22:22:20 +0530

    CalendarDateTile component completed

--- src/components/CalendarDateTile.tsx
+++ src/components/CalendarDateTile.tsx
@@ -1,13 +1,75 @@
 import { tw } from "@lib";
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
-export const CalendarDateTile = () => {
+type CalendarTileModeType = "default" | "selected" | "disabled";
+
+export interface ICalendarTile {
+  day: string;
+  date: number;
+  month: string;
+  mode?: CalendarTileModeType;
+}
+const NonTouchableCalendarTile = ({
+  day,
+  date,
+  month,
+  mode = "default",
+}: ICalendarTile) => {
   return (
-    <View style={tw`px-4 py-2 items-center flex-col`}>
-      <Text>Day</Text>
-      <Text>Date</Text>
-      <Text>Month</Text>
+    <View
+      style={tw`px-3.7 pt-2 pb-5 items-center ${
+        mode === "selected" ? "bg-pink" : ""
+      }`}
+    >
+      <Text
+        style={tw`text-sm ${
+          mode === "selected" ? "text-white" : "text-gray-500"
+        }`}
+      >
+        {day}
+      </Text>
+      <Text
+        style={tw`text-xl font-roboto-regular ${
+          mode === "disabled"
+            ? "text-gray-500"
+            : mode === "selected"
+            ? "text-white"
+            : "text-slate-800"
+        }`}
+      >
+        {date}
+      </Text>
+      <Text
+        style={tw`${
+          mode === "selected" ? "text-white" : "text-gray-500"
+        } text-sm `}
+      >
+        {month}
+      </Text>
     </View>
+  );
+};
+
+export const CalendarDateTile = ({ day, date, month, mode }: ICalendarTile) => {
+  if (mode === "disabled") {
+    return (
+      <NonTouchableCalendarTile
+        day={day}
+        month={month}
+        date={date}
+        mode={mode}
+      />
+    );
+  }
+  return (
+    <TouchableOpacity>
+      <NonTouchableCalendarTile
+        day={day}
+        month={month}
+        date={date}
+        mode={mode}
+      />
+    </TouchableOpacity>
   );
 };
