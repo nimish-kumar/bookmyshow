181bbb91e2b3101f0cd416c00a8b29e0b6b0d511
Nimish Kumar <nimishk642@gmail.com>
2023-04-03 21:00:31 +0530

    Added 'sold' style to the Seat component

--- src/components/seats/Seat.tsx
+++ src/components/seats/Seat.tsx
@@ -20,12 +20,14 @@ export const Seat = ({ seatNumber, status, seatSelectHandler }: ISeatProps) => {
       style={[
         tw`bg-white mr-1 h-6 w-6 border border-green justify-center items-center rounded-sm`,
         tw`${status === "selected" ? "bg-green" : ""}`,
+        tw`${status === "sold" ? "border-gray-400" : ""}`,
       ]}
     >
       <Text
         style={[
           tw`text-xs text-green`,
           tw`${status === "selected" ? " text-white" : ""}`,
+          tw`${status === "sold" ? "text-gray-500" : ""}`,
         ]}
       >
         {seatNumber}
