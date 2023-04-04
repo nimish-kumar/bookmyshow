0d38ca36cbb1770d9499769b08114bc3e9469b8d
Nimish Kumar <nimishk642@gmail.com>
2023-04-04 14:19:48 +0530

    Styling changes applied to SeatRowHeader

--- src/components/seats/SeatRowHeader.tsx
+++ src/components/seats/SeatRowHeader.tsx
@@ -10,11 +10,11 @@ export const SeatRowHeader: React.FC<
   PropsWithChildren<ISeatRowHeaderProps>
 > = ({ grpName, cost, children }) => {
   return (
-    <View style={tw`justify-start mt-2 mb-2 px-6`}>
+    <View style={tw`justify-start mt-4 mb-2 px-6`}>
       <Text
         style={tw`text-gray-500 text-sm mb-1`}
-      >{`${grpName} Rs. ${cost}`}</Text>
-      <View style={tw`border-t border border-gray-500`} />
+      >{`${grpName}      Rs. ${cost}`}</Text>
+      <View style={tw`border-t border-gray-300`} />
       <View style={tw`justify-center pt-2`}>{children}</View>
     </View>
   );
