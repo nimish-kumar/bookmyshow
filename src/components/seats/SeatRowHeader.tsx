16bff4294117f271dcc2a698335ea98b8331d214
Nimish Kumar <nimishk642@gmail.com>
2023-04-04 13:16:21 +0530

    Changed font size for seat row header

--- src/components/seats/SeatRowHeader.tsx
+++ src/components/seats/SeatRowHeader.tsx
@@ -11,7 +11,9 @@ export const SeatRowHeader: React.FC<
 > = ({ grpName, cost, children }) => {
   return (
     <View style={tw`justify-start mt-2 mb-2 px-6`}>
-      <Text style={tw`text-gray-500 mb-2`}>{`${grpName} - Rs. ${cost}`}</Text>
+      <Text
+        style={tw`text-gray-500 text-sm mb-1`}
+      >{`${grpName} Rs. ${cost}`}</Text>
       <View style={tw`border-t border border-gray-500`} />
       <View style={tw`justify-center pt-2`}>{children}</View>
     </View>
