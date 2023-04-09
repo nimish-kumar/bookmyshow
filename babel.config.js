fce907541bc7b10b51719d881c685d184415aa38
Nimish Kumar <nimishk642@gmail.com>
2023-04-09 21:11:15 +0530

    Added types folder to alias

--- babel.config.js
+++ babel.config.js
@@ -14,6 +14,7 @@ module.exports = function (api) {
             "@navigation": "./src/navigation",
             "@context": "./src/context",
             "@utils": "./src/utils",
+            "@types": "./src/types",
           },
         },
       ],
