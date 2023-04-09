12a285a24ca0ba6809cf64ce9153b93149ca4448
Nimish Kumar <nimishk642@gmail.com>
2023-04-09 11:52:15 +0530

    Minor change

--- src/components/seats/Gap.tsx
+++ src/components/seats/Gap.tsx
@@ -2,8 +2,8 @@ import { tw } from "@lib";
 import React from "react";
 import { View } from "react-native";
 
-const GapMemoized = () => {
+const NonMemoizedGap = () => {
   return <View style={tw`h-6 w-6 mr-1`} />;
 };
 
-export const Gap = React.memo(GapMemoized);
+export const Gap = React.memo(NonMemoizedGap);
