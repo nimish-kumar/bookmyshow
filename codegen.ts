3b2277dca6cd520933d1435f3ef7f26746eeadc1
Nimish Kumar <nimishk642@gmail.com>
2023-04-14 02:30:21 +0530

    GRAPHQL_API_URL added

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: "http://127.0.0.1:8000/gql/",
+  schema: "<GRAPHQL_API_URL>",
   documents: ["src/**/*.tsx"],
   generates: {
     "./src/__generated__/": {
