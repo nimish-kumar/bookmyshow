c4760a9d8002d1da7e7476b8d19693213ff82019
Nimish Kumar <nimishk642@gmail.com>
2023-04-15 15:48:03 +0530

    Added utilities for authentication

--- src/utils/authentication.ts
+++ src/utils/authentication.ts
@@ -1,6 +1,26 @@
-export const fetchToken = () => {};
-export const refreshToken = () => {};
-export const hasTokenExpired = () => {};
-export const setToken = () => {};
-export const getToken = () => {};
-export const isTokenNull = () => {};
+import dayjs from "dayjs";
+import * as SecureStore from "expo-secure-store";
+import jwtDecode, { JwtPayload } from "jwt-decode";
+
+export const hasTokenExpired = (token: string) => {
+  const expTime = jwtDecode<JwtPayload>(token).exp;
+  if (expTime === undefined)
+    throw new Error("Expiry time not available on the token");
+  return dayjs(expTime) >= dayjs();
+};
+export const setAccessToken = async (token: string) => {
+  await SecureStore.setItemAsync("accessToken", token);
+};
+export const getAccessToken = async () => {
+  const result = await SecureStore.getItemAsync("accessToken");
+  if (result) return result;
+  return null;
+};
+export const setRefreshToken = async (token: string) => {
+  await SecureStore.setItemAsync("refreshToken", token);
+};
+export const getRefreshToken = async () => {
+  const result = await SecureStore.getItemAsync("refreshToken");
+  if (result) return result;
+  return null;
+};
