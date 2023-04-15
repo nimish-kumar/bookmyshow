4c85d506dff2f040eb0e2d509da6191145f9e03d
Nimish Kumar <nimishk642@gmail.com>
2023-04-16 04:09:27 +0530

    Updated SignIn screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,16 +1,17 @@
-import { Text } from "@rneui/themed";
+import { Button, Text } from "@rneui/themed";
 import { tw } from "@tailwind";
 import React from "react";
-import { TouchableOpacity, View } from "react-native";
+import { View } from "react-native";
 
-export const SignIn = () => {
+interface ISignInProps {
+  handleLogin: () => void;
+}
+export const SignIn = ({ handleLogin }: ISignInProps) => {
   return (
-    <View style={tw`flex-col`}>
-      <TouchableOpacity style={tw`w-4 h-2`}>
-        <View style={tw`flex`}>
-          <Text style={tw``}> SignIn</Text>
-        </View>
-      </TouchableOpacity>
+    <View style={tw`flex-1 bg-red-400`}>
+      <Button style={tw`w-14 h-4`} onPress={handleLogin}>
+        <Text style={tw`text-white`}> SignIn</Text>
+      </Button>
     </View>
   );
 };
