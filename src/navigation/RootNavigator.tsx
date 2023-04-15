d071bb2025f76de25aae59e65bdb172357b06e98
Nimish Kumar <nimishk642@gmail.com>
2023-04-16 04:05:02 +0530

    Added AuthNavigator and AppNavigator to the RootNavigator

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,11 +1,18 @@
+import { AuthContext } from "@context";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { FormatSelector, Home, SeatSelector, SlotSelector } from "@screens";
+import {
+  FormatSelector,
+  Home,
+  SeatSelector,
+  SlotSelector,
+  Startup,
+} from "@screens";
 import { RootStackParamList } from "@types";
-import React from "react";
+import React, { useContext } from "react";
 
 const RootStack = createNativeStackNavigator<RootStackParamList>();
 
-export const RootNavigator = () => {
+const AppNavigator = () => {
   return (
     <RootStack.Navigator initialRouteName="Home">
       <RootStack.Group>
@@ -18,4 +25,20 @@ export const RootNavigator = () => {
       </RootStack.Group>
     </RootStack.Navigator>
   );
+};
+
+const AuthNavigator = () => {
+  return (
+    <RootStack.Navigator initialRouteName="Startup">
+      <RootStack.Group>
+        <RootStack.Screen name="Startup" component={Startup} />
+      </RootStack.Group>
+    </RootStack.Navigator>
+  );
+};
+
+export const RootNavigator = () => {
+  const { isLoggedIn } = useContext(AuthContext);
+  // console.log("Is logged in? ---> ", isLoggedIn);
+  return !isLoggedIn ? <AuthNavigator /> : <AppNavigator />;
 };
