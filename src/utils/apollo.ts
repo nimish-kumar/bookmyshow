b4b190414fd6a165ef489daea5bef397cac7eb12
Nimish Kumar <nimishk642@gmail.com>
2023-04-16 04:10:55 +0530

    Added authentication middleware, withToken context, and responseLogMiddleware

--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -1,7 +1,44 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloLink,
+  HttpLink,
+  from,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { GRAPHQL_API_URL } from "@env";
 
+import { getAccessToken } from "./authentication";
+
+const httpLink = new HttpLink({ uri: GRAPHQL_API_URL });
+const withToken = setContext(async () => {
+  const accessToken = await getAccessToken();
+  return { accessToken };
+});
+const authMiddleware = new ApolloLink((operation, forward) => {
+  const { accessToken } = operation.getContext();
+  operation.setContext(({ headers = {} }) => {
+    const newHeaders = {
+      headers: {
+        ...headers,
+        Authorization: accessToken ? `JWT ${accessToken}` : null,
+      },
+    };
+
+    console.log("Headers", newHeaders);
+    return newHeaders;
+  });
+  return forward(operation);
+});
+const responseLogMiddleware = new ApolloLink((operation, forward) => {
+  return forward(operation).map((response) => {
+    console.log("Response error ---> ", response.errors);
+    console.log("Response data ---> ", response.data);
+    return response;
+  });
+});
+
 export const client = new ApolloClient({
-  uri: GRAPHQL_API_URL,
   cache: new InMemoryCache(),
+  link: from([withToken, authMiddleware, responseLogMiddleware, httpLink]),
 });
