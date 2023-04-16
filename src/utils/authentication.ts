aa65fab1c6bdce4a95f3615b26d5975eb8e42592
Nimish Kumar <nimishk642@gmail.com>
2023-04-16 12:41:04 +0530

    Added isPastOrEqual, getRefreshExpiryTime and setRefreshExpiryTime under authentication utilities

--- src/utils/authentication.ts
+++ src/utils/authentication.ts
@@ -3,13 +3,14 @@ import dayjs from "dayjs";
 import * as SecureStore from "expo-secure-store";
 import jwtDecode from "jwt-decode";
 
+export const isPastOrEqual = (exp: number) => dayjs(exp * 1000) <= dayjs();
 export const hasTokenExpired = (token: string) => {
   const decodedJwt = jwtDecode<IPayload>(token);
   const expTime = decodedJwt.exp;
 
   if (expTime === undefined)
     throw new Error("Expiry time not available on the token");
-  return dayjs(expTime * 1000) <= dayjs();
+  return isPastOrEqual(expTime);
 };
 export const setAccessToken = async (token: string) => {
   await SecureStore.setItemAsync("accessToken", token);
@@ -26,4 +27,12 @@ export const getRefreshToken = async () => {
   const result = await SecureStore.getItemAsync("refreshToken");
   if (result) return result;
   return null;
+};
+export const getRefreshExpiryTime = async () => {
+  const result = await SecureStore.getItemAsync("refreshExpiresIn");
+  if (result) return parseInt(result, 10);
+  return null;
+};
+export const setRefreshExpiryTime = async (expTimestamp: number) => {
+  await SecureStore.setItemAsync("refreshExpiresIn", `${expTimestamp}`);
 };
