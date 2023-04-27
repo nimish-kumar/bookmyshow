8fe0f9acda7de1baed0ab3ee29d655b127c98233
Nimish Kumar <nimishk642@gmail.com>
2023-04-27 20:08:43 +0530

    TabBar styles added

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -1,6 +1,8 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { useNavigation } from "@react-navigation/native";
+import { Icon } from "@rneui/themed";
 import { Home, Tickets } from "@screens";
+import { tw } from "@tailwind";
 import React, { useLayoutEffect } from "react";
 export type TabNavigatorParamsList = {
   Home: undefined;
@@ -18,7 +20,25 @@ export const TabNavigator = () => {
     });
   }, []);
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route, navigation }) => ({
+        tabBarIcon: ({ color, focused, size }) => {
+          if (route.name === "Home") {
+            if (focused)
+              return <Icon type="entypo" name="home" color="white" />;
+            return <Icon type="entypo" name="home" color="#6B7280" />;
+          }
+          if (route.name === "Tickets") {
+            if (focused)
+              return <Icon type="fontisto" name="ticket" color="white" />;
+            return <Icon type="fontisto" name="ticket" color="#6B7280" />;
+          }
+        },
+        tabBarLabel: () => null,
+        tabBarActiveBackgroundColor: "#DC3558",
+        tabBarStyle: tw`h-10`,
+      })}
+    >
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Tickets" component={Tickets} />
     </Tab.Navigator>
