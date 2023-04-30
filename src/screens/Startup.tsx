6660469f2022f730ae38620f634dd220487f66fd
Nimish Kumar <nimishk642@gmail.com>
2023-04-30 21:41:25 +0530

    Added Google sign in functionality by firebase

--- src/screens/Startup.tsx
+++ src/screens/Startup.tsx
@@ -1,19 +1,15 @@
-import { useMutation } from "@apollo/client";
 import { AuthContext } from "@context";
-import { EMAIL_ID, PASSWORD } from "@env";
-import { FETCH_ACCESS_TOKEN, REFRESH_TOKEN } from "@graphql";
+import { FIREBASE_WEB_CLIENT_ID } from "@env";
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
 import { useNavigation } from "@react-navigation/native";
 import { tw } from "@tailwind";
 import { StartupNavigationProp } from "@types";
 import {
   getAccessToken,
-  getRefreshExpiryTime,
-  getRefreshToken,
+  googleSignIn,
   hasTokenExpired,
-  isPastOrEqual,
   setAccessToken,
-  setRefreshExpiryTime,
-  setRefreshToken,
   setUserDetails,
 } from "@utils";
 import React, { useContext, useEffect, useLayoutEffect, useState } from "react";
@@ -24,6 +20,17 @@ import { Splash } from "./Splash";
 
 export const Startup = () => {
   const { setLoggedIn } = useContext(AuthContext);
+  useEffect(() => {
+    console.log("FIREBASE_WEB_CLIENT_ID", FIREBASE_WEB_CLIENT_ID);
+    GoogleSignin.configure({
+      scopes: [
+        "email",
+        "profile",
+        "https://www.googleapis.com/auth/user.gender.read",
+      ],
+      webClientId: FIREBASE_WEB_CLIENT_ID,
+    });
+  }, []);
   const [splashVisible, setSplashVisibility] = useState(true);
   const navigation = useNavigation<StartupNavigationProp>();
   useLayoutEffect(() => {
@@ -31,153 +38,54 @@ export const Startup = () => {
       headerShown: false,
     });
   });
-  const [
-    fetchLoginToken,
-    {
-      loading: fetchingAccessToken,
-      error: accessTokenError,
-      data: accessTokenData,
-    },
-  ] = useMutation(FETCH_ACCESS_TOKEN, {
-    variables: { email: EMAIL_ID, password: PASSWORD },
-    fetchPolicy: "no-cache",
-  });
 
-  const [
-    fetchRefreshedToken,
-    {
-      loading: fetchingRefreshToken,
-      error: refreshTokenError,
-      data: refreshTokenData,
-    },
-  ] = useMutation(REFRESH_TOKEN, { fetchPolicy: "no-cache" });
   useEffect(() => {
-    (async () => {
-      try {
-        let accessToken: string | null = null;
-        let refreshToken: string | null = null;
-        let refreshExpiryTime: number | null = null;
-        accessToken = await getAccessToken();
-        if (accessToken) {
-          if (hasTokenExpired(accessToken)) {
-            refreshToken = await getRefreshToken();
-            refreshExpiryTime = await getRefreshExpiryTime();
+    let token: string | null = null;
+    getAccessToken().then((t) => {
+      if (!t) {
+        setSplashVisibility(false);
+      }
+      token = t;
+    });
 
-            if (
-              (refreshExpiryTime && isPastOrEqual(refreshExpiryTime)) ||
-              refreshExpiryTime === null
-            ) {
-              setLoggedIn(false);
-              setSplashVisibility(false);
-            } else {
-              fetchRefreshedToken({
-                variables: { refreshToken },
-              });
-            }
-          } else {
-            // If access token has not expired, simply login
-            setLoggedIn(true);
+    const subscriber = auth().onAuthStateChanged(
+      async (user: FirebaseAuthTypes.User | null) => {
+        if (user) {
+          console.log("User");
+          const promiseSetUser = await setUserDetails(
+            user.displayName,
+            user.email || "",
+            user.photoURL
+          );
+          // If a token has expired or no token
+          if (!token || hasTokenExpired(token)) {
+            token = await user.getIdToken(true);
           }
-        } else {
-          // If token was not found set splash visibility false.
-          // Because of which we will see SignIn page
-          setSplashVisibility(false);
-        }
-      } catch (error) {
-        if (error) {
-          console.error("Error: ", error);
-          throw Error("StorageError: Error while reading tokens");
+          const promiseSetAccessToken = await setAccessToken(token);
+          Promise.all([promiseSetUser, promiseSetAccessToken]).then(() => {
+            //Set splash visibility to false
+            setSplashVisibility(false);
+            // Route to application route
+            setLoggedIn(true);
+          });
         }
       }
-    })();
+    );
+    return subscriber; // unsubscribe on unmount
   }, []);
-  useEffect(() => {
-    if (!fetchingAccessToken && accessTokenData?.tokenAuth) {
-      const atPromise = setAccessToken(accessTokenData.tokenAuth.token);
-      const rtPromise = setRefreshToken(accessTokenData.tokenAuth.refreshToken);
-      const userDetailsPromise = setUserDetails(
-        null,
-        accessTokenData.tokenAuth.payload["email"]
-      );
-      const rtExpiryTimePromise = setRefreshExpiryTime(
-        accessTokenData.tokenAuth.refreshExpiresIn
-      );
-      Promise.all([
-        atPromise,
-        rtPromise,
-        rtExpiryTimePromise,
-        userDetailsPromise,
-      ])
-        .then(() => {
-          setSplashVisibility(true);
-          setLoggedIn(true);
-        })
-        .catch((err) => {
-          // This is used to switch back navigators
-          // to Auth
-          setLoggedIn(false);
-          // User should now see login screen
-          setSplashVisibility(false);
-          console.error("Error: ", err);
-          throw Error("StorageError: Error while setting tokens");
-        });
-    }
-    if (!fetchingAccessToken && accessTokenError) {
-      setLoggedIn(false);
-      setSplashVisibility(false);
-      throw Error(
-        "APIError: Could not fetch results for FETCH_ACCESS_TOKEN api"
-      );
-    }
-  }, [fetchingAccessToken]);
-
-  useEffect(() => {
-    if (
-      refreshTokenData?.refreshToken &&
-      refreshTokenData.refreshToken.token &&
-      refreshTokenData.refreshToken.refreshToken
-    ) {
-      const atPromise = setAccessToken(refreshTokenData.refreshToken.token);
-      const rtPromise = setRefreshToken(
-        refreshTokenData.refreshToken.refreshToken
-      );
-      const userDetailsPromise = setUserDetails(
-        null,
-        refreshTokenData.refreshToken.payload["email"]
-      );
-      const rtExpiryTimePromise = setRefreshExpiryTime(
-        refreshTokenData.refreshToken.refreshExpiresIn
-      );
-      Promise.all([
-        atPromise,
-        rtPromise,
-        rtExpiryTimePromise,
-        userDetailsPromise,
-      ])
-        .then(() => {
-          setSplashVisibility(true);
-          setLoggedIn(true);
-        })
-        .catch((err) => {
-          setLoggedIn(false);
-          setSplashVisibility(false);
-          console.error("Error: ", err);
-          throw Error("StorageError: Error while setting tokens");
-        });
-    }
-    if (!fetchingRefreshToken && refreshTokenError) {
-      setLoggedIn(false);
-      setSplashVisibility(false);
-      throw Error("APIError: Could not fetch results for REFRESH_TOKEN API");
-    }
-  }, [fetchingRefreshToken]);
 
   if (splashVisible) {
     return <Splash />;
   }
   return (
     <View style={tw`flex-1`}>
-      <SignIn handleLogin={fetchLoginToken} />
+      <SignIn
+        handleLogin={() =>
+          googleSignIn()
+            .then(() => setSplashVisibility(true))
+            .catch(() => setSplashVisibility(false))
+        }
+      />
     </View>
   );
 };
--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -2,4 +2,5 @@ declare module "@env" {
   export const GRAPHQL_API_URL: string;
   export const EMAIL_ID: string;
   export const PASSWORD: string;
+  export const FIREBASE_WEB_CLIENT_ID: string;
 }
