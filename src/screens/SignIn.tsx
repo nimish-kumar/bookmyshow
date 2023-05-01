402286c1512ed89ca36fa0e807b126929605d1de
Nimish Kumar <nimishk642@gmail.com>
2023-05-01 15:53:16 +0530

    Added BookMyShow Icon and redesigned login with google button

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,16 +1,24 @@
-import { Button, Text } from "@rneui/themed";
+import { BookMyShowDarkSvg, GoogleSvg } from "@assets";
+import { Button } from "@rneui/themed";
 import { tw } from "@tailwind";
 import React from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 interface ISignInProps {
   handleLogin: () => void;
 }
 export const SignIn = ({ handleLogin }: ISignInProps) => {
   return (
-    <View style={tw`flex-1 bg-red-400`}>
-      <Button style={tw`w-14 h-4`} onPress={handleLogin}>
-        <Text style={tw`text-white`}> SignIn</Text>
+    <View style={tw`flex-1 bg-white pt-40 items-center px-8`}>
+      <BookMyShowDarkSvg height={180} width={180} />
+      <Button
+        onPress={handleLogin}
+        buttonStyle={tw`h-12 w-full bg-white border border-black flex-row px-10 mt-10`}
+      >
+        <View style={tw`flex-row justify-center`}>
+          <GoogleSvg height={20} width={20} />
+          <Text style={tw`px-8`}>Continue with Google</Text>
+        </View>
       </Button>
     </View>
   );
