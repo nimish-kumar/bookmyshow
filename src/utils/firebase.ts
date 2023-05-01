0964c8967e378448bdff1b2b001fa87f7377eb84
Nimish Kumar <nimishk642@gmail.com>
2023-05-02 02:04:52 +0530

    Added googleSignInSilently function for refetching token if it expires

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -35,6 +35,35 @@ export const googleSignIn = async () => {
   }
 };
 
+export const googleSignInSilently = async () => {
+  try {
+    await GoogleSignin.hasPlayServices();
+    const { idToken } = await GoogleSignin.signInSilently();
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  } catch (error) {
+    const typedError = error as NativeModuleError;
+
+    switch (typedError.code) {
+      case statusCodes.SIGN_IN_CANCELLED:
+        // sign in was cancelled
+        break;
+      case statusCodes.IN_PROGRESS:
+        // operation (eg. sign in) already in progress
+        break;
+      case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+        // android only
+        break;
+      default:
+        return {
+          userInfo: null,
+          error: typedError,
+        };
+    }
+  }
+};
 export const googleSignOut = async (token: string) => {
   try {
     await GoogleSignin.clearCachedAccessToken(token);
