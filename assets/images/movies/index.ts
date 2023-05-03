ef2f286b7f05626010512e6adfedc55e5fa720cf
Nimish Kumar <nimishk642@gmail.com>
2023-05-03 17:55:29 +0530

    [CLEANUP] Removed movie posters images

--- assets/images/movies/index.ts
+++
@@ -1,7 +0,0 @@
-export const AvatarTile = require("./avatar.jpg");
-export const BlackAdamTile = require("./black-adam.jpg");
-export const KutteyTile = require("./kuttey.jpg");
-export const LakadbaggaTile = require("./lakadbagga.jpg");
-export const TopGunTile = require("./top-gun.jpg");
-export const ValviTile = require("./valvi.jpg");
-export const VedTile = require("./ved.jpg");
