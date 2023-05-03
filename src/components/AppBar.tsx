9205e6a3719712d624f2eb194b1c07e029f13bff
Nimish Kumar <nimishk642@gmail.com>
2023-05-03 13:43:17 +0530

    Changed font to monsterrat

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -32,7 +32,7 @@ const NonMemoizedAppBar = ({
       ) : null}
       <View style={tw`justify-center w-2/3`}>
         <Text
-          style={tw`text-lg text-white font-roboto-bold`}
+          style={tw`text-lg text-white font-montserrat-bold`}
           numberOfLines={1}
           ellipsizeMode="tail"
         >
