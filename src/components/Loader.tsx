0770cbd1463c926ba6917c770b379a437e897a73
Nimish Kumar <nimishk642@gmail.com>
2023-05-03 16:19:47 +0530

    Added Loader component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { ActivityIndicator, StyleProp, ViewStyle } from "react-native";
+interface ILoaderProps {
+  size: "sm" | "md" | "lg" | "xl";
+  style: StyleProp<ViewStyle>;
+}
+export const Loader = ({ size = "md", style }: ILoaderProps) => {
+  if (size === "sm")
+    return <ActivityIndicator size={25} color="red" style={style} />;
+
+  if (size === "lg")
+    return <ActivityIndicator size={75} color="red" style={style} />;
+
+  if (size === "xl")
+    return <ActivityIndicator size={100} color="red" style={style} />;
+
+  return <ActivityIndicator size={50} color="red" />;
+};
